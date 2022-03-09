0c626c06a5ecb59a5e232f6d83bce694ec54c07b
Apollo Tang <tang.apollo@gmail.com>
2022-03-09 16:41:46 -0500

    update folder name to reflect order

---
+++ wf-alphs/1-babel7/src/export-import.ts
@@ -0,0 +1,8 @@
+import { someFunction } from './some-module';
+import { foo } from './some-module';
+//       ^^^
+//       what is foo? what are we importing?
+
+export { someFunction };
+export { foo };
+
---
+++ wf-alphs/1-babel7/src/some-module.ts
@@ -0,0 +1,5 @@
+const someFunction = () => 'return of someFunction'
+type foo = string
+
+export { someFunction }
+export { foo }
