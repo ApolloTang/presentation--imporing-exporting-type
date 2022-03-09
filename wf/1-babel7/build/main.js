74c583b8383417b22264e9b80eb2de08ee5bc4fb
Apollo Tang <tang.apollo@gmail.com>
2022-03-09 14:30:58 -0500

    remove build folder

--- wf/1-babel7/build/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _exportImport = require("./export-import");
-
-console.log('someFunction(): ', (0, _exportImport.someFunction)());
-console.log('foo: ', _exportImport.foo);
-console.log('finished execution');
