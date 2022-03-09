b7d7b309d61bfe9b0328c6232ae6229ecb60fc6d
Apollo Tang <tang.apollo@gmail.com>
2022-03-09 16:32:52 -0500

    renaming folder

--- wf/2-tsc/build/some-module.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.someFunction = void 0;
-var someFunction = function () { return 'return of someFunction'; };
-exports.someFunction = someFunction;
