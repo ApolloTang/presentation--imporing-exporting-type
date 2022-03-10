a50fb738a67579fb12979a9a75239675a0dbf73e
Apollo Tang <tang.apollo@gmail.com>
2022-03-10 02:33:37 -0500

    remove comment

--- wf-omega/2-tsc/src/export-import.ts
+++ wf-omega/2-tsc/src/export-import.ts
@@ -1,7 +1,5 @@
 import { someFunction } from './some-module';
 import { foo } from './some-module';
-//       ^^^
-//       what is foo? what are we importing?
 
 export { someFunction };
 export { foo };
