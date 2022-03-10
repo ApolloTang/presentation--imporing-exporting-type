ceff056d3abde654c8cf301b861ce13b30b83ad9
Apollo Tang <tang.apollo@gmail.com>
2022-03-10 01:35:17 -0500

    remove comment

--- wf-alpha/2-tsc/src/main.ts
+++ wf-alpha/2-tsc/src/main.ts
@@ -2,5 +2,5 @@ import { someFunction } from './export-import'
 import { foo } from './export-import'
 
 console.log('someFunction(): ', someFunction())
-console.log('foo: ', foo)  // <--- this is a type
+console.log('foo: ', foo)
 console.log('finished execution')
